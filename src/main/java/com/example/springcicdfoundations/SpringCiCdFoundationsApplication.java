55c1008961ee9438fad6b4f61aab32d05717a951
DevRamona <r.ingabire@alustudent.com>
2026-02-04 15:57:05 +0200

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/springcicdfoundations/SpringCiCdFoundationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcicdfoundations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiCdFoundationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCiCdFoundationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcicdfoundations/SpringCiCdFoundationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcicdfoundations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCiCdFoundationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
