e5cd3a523bf755bed5dba70489e6657d35719565
DevRamona <r.ingabire@alustudent.com>
2026-02-06 11:29:01 +0200

    fix: remove the failing test

---
+++ src/test/java/com/example/springcicdfoundations/FailingTest.java
@@ -0,0 +1,12 @@
+package com.example.springcicdfoundations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FailingTest {
+    @Test
+    void failingTest() {
+        assertEquals(1,1);
+    }
+}
